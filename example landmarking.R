ed33276bc7da604e86ff0fd08538da3273b271e5
anamariapatron <anamariapatron_75@hotmail.com>
2025-09-01 01:23:03 +0100

    updates

--- example landmarking.R
+++ example landmarking.R
@@ -17,11 +17,11 @@ data_repeat_outcomes <-
 data_model_landmark_LOCF <-
   fit_LOCF_landmark(
     data_long = data_repeat_outcomes,
-    x_L = c(60, 61),
-    x_hor = c(65, 66),
+    x_L = c(60, 61, 62),
+    x_hor = c( 60, 61,62)+1,
     covariates = c("ethnicity", "smoking", "diabetes", "sbp_stnd", "tchdl_stnd"),
     covariates_time = c(rep("response_time_sbp_stnd", 4), "response_time_tchdl_stnd"),
-    k = 10,
+    k = 2,
     individual_id = "id",
     event_time = "event_time",
     event_status = "event_status",
@@ -37,3 +37,65 @@ plot(
 data_model_landmark_LOCF[["60"]]$data$event_prediction
 data_model_landmark_LOCF[["60"]]$data$event_time
 
+
+data_model_landmark_LOCF[["61"]]$data$event_prediction
+
+
+#######################################
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+
+# Supongamos que queremos la persona con id = 2
+person_id <- 2
+
+# Seleccionamos los landmarks que tenemos
+LMs <- names(data_model_landmark_LOCF)
+
+# Crear dataframe con cumulative hazard para esta persona en cada landmark
+cumhaz_person <- lapply(LMs, function(LM) {
+  df_lm <- data_model_landmark_LOCF[[LM]]$data
+  df_person <- df_lm %>% filter(id == person_id)
+  if(nrow(df_person) == 0) return(NULL)
+  
+  data.frame(
+    LM = as.numeric(LM),
+    cumhaz = -log(1 - df_person$event_prediction)
+  )
+}) %>% bind_rows()
+
+# Graficar
+ggplot(cumhaz_person, aes(x = LM, y = cumhaz)) +
+  geom_point(size = 3, color = "blue") +
+  geom_line(color = "blue", lwd = 1) +
+  labs(
+    x = "Landmark time",
+    y = "Cumulative hazard",
+    title = paste("Cumulative hazard for person", person_id)
+  ) +
+  theme_minimal()
+
+#######################################
+
+
+
+data_model_landmark_LME <-
+  fit_LME_landmark(
+    data_long = data_repeat_outcomes[["60"]],
+    x_L = c(60),
+    x_hor = c(65),
+    cross_validation_df =
+      cross_validation_list,
+    fixed_effects = c("ethnicity", "smoking", "diabetes"),
+    fixed_effects_time =
+      "response_time_sbp_stnd",
+    random_effects = c("sbp_stnd", "tchdl_stnd"),
+    random_effects_time = c("response_time_sbp_stnd", "response_time_tchdl_stnd"),
+    individual_id = "id",
+    standardise_time = TRUE,
+    lme_control = nlme::lmeControl(maxIter =
+                                     100, msMaxIter = 100),
+    event_time = "event_time",
+    event_status = "event_status",
+    survival_submodel = "cause_specific"
+  )
